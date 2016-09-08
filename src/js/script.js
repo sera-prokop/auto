50683f2079cdd83145b559ba086b546ae7c1b3b8
sera.prokop <sera.prokop@gmail.com>
2016-09-08 23:27:23 +0300

    slider

--- src/js/script.js
+++ src/js/script.js
@@ -4,7 +4,27 @@
 //
 // });
 
-var swiper = new Swiper('.swiper-container', {
-    pagination: '.swiper-pagination',
-    paginationClickable: true
+var swiper = new Swiper('.header-slider', {
+    pagination: '.header-slider__toggle',
+    paginationClickable: true,
+    paginationCustomRender: function (swiper, current, total) {
+      return current + ' of ' + total;
+  },
+});
+
+var swiperTwo = new Swiper('.offers__slider', {
+    slidesPerView: '3',
+    spaceBetween: 50,
+    nextButton: '.swiper-button-next',
+    prevButton: '.swiper-button-prev',
+    breakpoints: {
+      768: {
+          slidesPerView: 2,
+          spaceBetween: 30
+      },
+      640: {
+          slidesPerView: 1,
+          spaceBetween: 10
+      }
+    }
 });
